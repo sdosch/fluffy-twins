f3ee40dd388a3be5eac17f76ff625bbbf48f90d5
Stefan Dosch <sd@Stefans-MacBook-Pro.local>
2019-09-27 03:47:11 +0200

    Updates

---
+++ precache-manifest.9f028728b43fca5eb5271cf34ba3cc9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bf56928816fce3163b52fe7d753e3e",
+    "url": "/fluffy-twins/index.html"
+  },
+  {
+    "revision": "0588c6055eca4e043877",
+    "url": "/fluffy-twins/static/css/main.e4ee0d20.chunk.css"
+  },
+  {
+    "revision": "b1fd4b2bfd3194cf5992",
+    "url": "/fluffy-twins/static/js/2.d2733273.chunk.js"
+  },
+  {
+    "revision": "0588c6055eca4e043877",
+    "url": "/fluffy-twins/static/js/main.cfba7c17.chunk.js"
+  },
+  {
+    "revision": "9bb79180e6bd492a0790",
+    "url": "/fluffy-twins/static/js/runtime-main.a3df07bd.js"
+  },
+  {
+    "revision": "5695bb1a3e5c45294f5240d149f028d7",
+    "url": "/fluffy-twins/static/media/pawprint.5695bb1a.svg"
+  }
+]);
